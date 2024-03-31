5bbae7acde4dadd12cf99dd12694b534ee545fd2
Zun Lin <zunliang2008@gmail.com>
2024-03-31 16:54:18 -0400

    add invoices

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,15 +6,6 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, InvoiceSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 export default async function Page() {
-
-    // const revenue = await fetchRevenue();
-    // const latestInvoices = await fetchLatestInvoices();
-    // const {
-    //     numberOfInvoices,
-    //     numberOfCustomers,
-    //     totalPaidInvoices,
-    //     totalPendingInvoices,
-    // } = await fetchCardData();
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
